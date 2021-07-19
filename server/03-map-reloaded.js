8946f724cbb1723d3ee6588659b916f1a5d2e1f3
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-19 12:15:35 -0400

    :construction: work

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,40 @@
+const orders = [
+  {
+    customerName: "Nicolas",
+    total: 60,
+    delivered: true,
+  },
+  {
+    customerName: "Zulema",
+    total: 120,
+    delivered: false,
+  },
+  {
+    customerName: "Santiago",
+    total: 180,
+    delivered: true,
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true,
+  },
+];
+console.log("original", orders);
+const rta = orders.map((item) => item.total);
+console.log("rta", rta);
+
+// const rta2 = orders.map((item) => {
+//   item.tax = 0.19;
+//   return item;
+// });
+// console.log("rta2", rta2);
+// console.log("original", orders);
+const rta3 = orders.map((item) => {
+  return {
+    ...item,
+    tax: 0.19,
+  };
+});
+console.log("rta3", rta3);
+console.log("original", orders);
