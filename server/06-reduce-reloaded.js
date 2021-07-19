70c3ff1875eb08634a57e10ea5fa475dea923e0e
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-19 13:43:21 -0400

    :construction: work in reduce

---
+++ server/06-reduce-reloaded.js
@@ -0,0 +1,52 @@
+const items = [1, 3, 2, 3];
+
+const rta = items.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(rta);
+
+const data = [
+  {
+    name: "Nicolas",
+    level: "low",
+  },
+  {
+    name: "Andrea",
+    level: "medium",
+  },
+  {
+    name: "Zulema",
+    level: "hight",
+  },
+  {
+    name: "Santiago",
+    level: "low",
+  },
+  {
+    name: "Valentina",
+    level: "medium",
+  },
+  {
+    name: "Lucia",
+    level: "hight",
+  },
+];
+
+const rta1 = data
+.map(item => item.level)
+.reduce((obj, item) => {
+    if (!obj[item]) {
+        obj[item] = 1;
+    } else {
+        obj[item] = obj[item] + 1;
+    }
+    return obj;
+}, {});
+
+console.log(rta1);
