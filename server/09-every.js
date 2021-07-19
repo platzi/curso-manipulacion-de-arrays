4a3d6393dd56a60db071dafd28557107d3540664
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-19 14:32:28 -0400

    :construction: every

---
+++ server/09-every.js
@@ -0,0 +1,33 @@
+const numbers = [1,30,39,29,10,13];
+
+
+let rta = true;
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element >= 40) {
+        rta = false
+    }
+}
+console.log('for', rta);
+const rta2 = numbers.every(item => item <= 40)
+console.log('rta2', rta2);
+
+
+const team = [
+  {
+    name: "Nicolas",
+    age: 12,
+  },
+  {
+    name: "Andrea",
+    age: 8,
+  },
+  {
+    name: "Zulema",
+    age: 2,
+  },
+  {
+    name: "Santiago",
+    age: 18,
+  },
+];
