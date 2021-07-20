ce7f3b28f248d88ed7f74771dcffd285bf4162a6
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-20 14:28:26 -0400

    :construction: implement includes

---
+++ server/11-includes.js
@@ -0,0 +1,14 @@
+const pets = ['cat', 'dog', 'bat'];
+
+let includeInArray = false;
+for (let index = 0; index < pets.length; index++) {
+    const element = pets[index];
+    if (element === 'dog') {
+        includeInArray = true;
+        break;
+    }
+}
+
+const rta = pets.includes('dog')
+console.log('for', includeInArray);
+console.log('includes', rta);
