ed083731d54d68ebe2a7764d7bb6b4b42140993d
Nicolas Molina Monroy <hi@nicobytes.com>
2021-07-20 15:01:53 -0400

    :construction: work

---
+++ server/13-concat.js
@@ -0,0 +1,18 @@
+const elements = [1,1,2,2];
+const othersElements = [3,3,4,4];
+
+const newArray = [...elements];
+for (let index = 0; index < othersElements.length; index++) {
+    const element = othersElements[index];
+    newArray.push(element);
+}
+
+const rta = elements.concat(othersElements);
+const rta2 = [...elements, ...othersElements];
+const rta3 = [...elements, ...'random'];
+console.log('for', newArray);
+console.log('concat', rta);
+console.log('...', rta2);
+console.log('rta3', rta3);
+elements.push(...othersElements);
+console.log('elements', elements);
