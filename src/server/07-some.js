f520a18ab16bd76cd51c36dce68fbef146dd3ee4
martin241125 <martincitooo21@gmail.com>
2023-08-18 21:02:51 -0300

    reactualizado

---
+++ src/server/07-some.js
@@ -0,0 +1,9 @@
+const numbers = [1, 2, 3, 5];
+
+//metodo some retorna true o false si se cumple la condicion.
+
+const withSome = numbers.some((item) => item % 2 === 0);
+
+console.log(withSome);
+
+
